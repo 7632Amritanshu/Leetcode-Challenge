31bfb8bd5f06e8ff57362649ef7638bbb4489a4a
Amritanshu Kumar <123amritanshu123@gmail.com>
2026-08-23 23:54:48 +0530

    Time: 8 ms (53.28%), Space: 47.2 MB (10.95%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        double sumDiff = 0;
+        double qDiff = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?') {
+                qDiff++;
+            } else {
+                sumDiff += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                qDiff--;
+            } else {
+                sumDiff -= num.charAt(i) - '0';
+            }
+        }
+
+        return sumDiff + (qDiff / 2.0) * 9 != 0;
+    }
+}
