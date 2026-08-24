049b75ad929933037606e65baca7f2bc34fef44e
Amritanshu Kumar <123amritanshu123@gmail.com>
2026-08-24 16:30:43 +0530

    Time: 0 ms (100%), Space: 43 MB (73.59%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -3,15 +3,17 @@ class Solution {
         if(word.length() <= 8 ) return word.length();
         int n= word.length();
         int sum =0;int i=1;
-        while(n> 0){
-            if(n>8){
-                sum += 8*i;
-            }else{
-                sum += n*i;
-            }
+        while(n> 8){
+            // if(n>8){
+            //     sum += 8*i;
+            // }else{
+            //     sum += n*i;
+            // }
+            sum += 8*i;
             n = n-8;
             i++;
         }
+        sum += n*i;
         return sum ;
     }
 }
